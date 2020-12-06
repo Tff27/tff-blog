88805862fc22c4e0183399bc136ea1c57fe78d6e
Tiago Fonseca <tiagoffonseca@gmail.com>
2020-12-06 17:42:18 +0000

    chore: add author view model

---
+++ tff-blog/ViewModels/Author.cs
@@ -0,0 +1,11 @@
+﻿namespace tffBlog.ViewModels
+{
+    public class Author
+    {
+        public string PhotoUrl { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
