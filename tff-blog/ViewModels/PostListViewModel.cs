359df6310e0a6941090c381c275a9b0b6637fe74
Tiago Fonseca <tiagoffonseca@outlook.pt>
2021-05-09 19:32:19 +0100

    feat: posts list close #7

---
+++ tff-blog/ViewModels/PostListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace tffBlog.ViewModels
+{
+    public class PostListViewModel
+    {
+        public PostListViewModel()
+        {
+            Posts = new List<PostViewModel>();
+        }
+
+        public IEnumerable<PostViewModel> Posts { get; set; }
+    }
+}
