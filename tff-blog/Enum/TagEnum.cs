05a0e6370bc1b45bb253da1d8272a92ab3d959fb
Tiago Fonseca <tiagoffonseca@gmail.com>
2021-05-09 19:33:49 +0100

    feat: posts list (#25)
    close #7

---
+++ tff-blog/Enum/TagEnum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace tffBlog.Enum
+{
+    public enum TagEnum
+    {
+        Net,
+        Azure,
+        JavaScript,
+        CI,
+        CD,
+        DevOps,
+        BotFramework
+    }
+}
---
+++ tff-blog/ViewModels/PostViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using tffBlog.Enum;
+
+namespace tffBlog.ViewModels
+{
+    public class PostViewModel
+    {
+        public PostViewModel()
+        {
+            Tags = new List<TagEnum>();
+        }
+
+        public string Image { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Title { get; set; }
+
+        public string Text { get; set; }
+
+        public IEnumerable<TagEnum> Tags { get; set; }
+    }
+}
