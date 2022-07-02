9497cf677bb1395601ba1926364a0e80a28bef98
Tiago Fonseca <tiagoffonseca@gmail.com>
2022-07-02 23:57:50 +0100

    feat: cosume data from API (#78)

--- tff-blog/Program.cs
+++ tff-blog/Program.cs
@@ -3,14 +3,14 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Ganss.XSS;
 using Microsoft.Extensions.DependencyInjection;
 using System;
-using System.Net.Http;
 using tffBlog;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddHttpClient("Client", sp => sp.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
+builder.Services.AddHttpClient("WebAPI", sp => sp.BaseAddress = new Uri(builder.Configuration["ApiUrl"]));
 
 builder.Services.AddScoped<IHtmlSanitizer, HtmlSanitizer>(x =>
 {
