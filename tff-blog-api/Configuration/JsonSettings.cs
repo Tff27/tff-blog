2f98b8b84fd060c5ed00adba01421e9c871527cc
Tiago Fonseca <tiagoffonseca@gmail.com>
2022-08-22 22:45:09 +0100

    chore: add author real info (#102)

---
+++ tff-blog-api/Configuration/JsonSettings.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json;
+
+namespace Tff.Blog.Api.Configuration
+{
+    public class JsonSettings
+    {
+        public static readonly JsonSerializerOptions Options = new()
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            PropertyNameCaseInsensitive = true,
+            WriteIndented = true,
+        };
+    }
+}
---
+++ tff-blog-shared/Converters/MarkdownToModelConverter.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.RegularExpressions;
+using Tff.Blog.Shared.Models;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+namespace Tff.Blog.Shared.Converters
+{
+    public static class MarkdownToModelConverter
+    {
+        public static T CreateModelFromMarkdown<T>(string markdown) where T : MarkdownModel
+        {
+            var YamlDeserializer = new DeserializerBuilder()
+              .WithNamingConvention(CamelCaseNamingConvention.Instance)
+              .IgnoreUnmatchedProperties()
+              .Build();
+
+            var expression = "(?:---\\r?\\n)(?<frontmatter>[\\s\\S]*?)(?:---\\r?\\n)";
+
+            var result = Regex.Match(markdown, expression).Groups.GetValueOrDefault("frontmatter").Value;
+            var model = YamlDeserializer.Deserialize<T>(result);
+
+            Regex regex = new(expression);
+            model.Text = (regex.Replace(markdown, string.Empty)).Trim();
+
+            return model;
+        }
+    }
+}
