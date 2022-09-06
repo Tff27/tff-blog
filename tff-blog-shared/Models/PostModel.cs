467c3c3fe57574c6ae9a6ad7dba2130a07fd23c2
Tiago Fonseca <tiagoffonseca@gmail.com>
2022-09-06 23:00:49 +0100

    chore: add description field

--- tff-blog-shared/Models/PostModel.cs
+++ tff-blog-shared/Models/PostModel.cs
@@ -15,6 +15,8 @@ namespace Tff.Blog.Shared.Models
 
         public string Title { get; set; }
 
+        public string Description { get; set; }
+
         public IEnumerable<TagEnum> Tags { get; set; }
 
         public string Slug { get; set; }
